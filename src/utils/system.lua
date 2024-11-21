d7b8f9676b4ef655a708c7628e39d9258b6541db
Balthazar <the.balthazar@Gmail.com>
2024-11-21 16:13:58 +0000

    osCall for win no longer goes through cmd

--- src/utils/system.lua
+++ src/utils/system.lua
@@ -1,7 +1,7 @@
 function osCall(call, options)
     local OS = love.system.getOS()
     if OS=='Windows' then
-        os.execute('start cmd /c call "'..call..'" '..(options or ''))
+        os.execute('start "" "'..call:gsub('/', '\\')..'" '..(options or ''))
     elseif OS=='OS X' or OS=='Linux' then
         os.execute('chmod +x "'..call..'" '..(options or ''))
     end
