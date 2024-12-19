b4796163e885c48714e8a247e5efe4b7a2b3fed3
Balthazar <the.balthazar@Gmail.com>
2024-12-19 14:37:16 +0000

    LoudDataPath error logs now red

--- src/utils/files/LoudDataPath.lua
+++ src/utils/files/LoudDataPath.lua
@@ -9,10 +9,10 @@ require'utils.filesystem'
 return function(stringData)
     local userConfig = userConfig or loadSaveFileData'userConfig' or {} -- this might be called from a thread that can't see globals.
     stringData, reps = stringData:gsub(maps, (not userConfig.docMaps and '--%1' or '%1'), 1)
-    if reps~=1 then feedback:push'Failed to update LoudDataPath.lua with doc maps config' end
+    if reps~=1 then feedback:push{{0.7, 0, 0.3}, 'Failed to update LoudDataPath.lua with doc maps config\n'} end
     stringData, reps = stringData:gsub(mods, (not userConfig.docMods and '--%1' or '%1'), 1)
-    if reps~=1 then feedback:push'Failed to update LoudDataPath.lua with doc mods config' end
+    if reps~=1 then feedback:push{{0.7, 0, 0.3}, 'Failed to update LoudDataPath.lua with doc mods config\n'} end
     stringData, reps = stringData:gsub(stratReg, stratForm:format(userConfig.iconSet or 'SMALL-classic'), 1)
-    if reps~=1 then feedback:push'Failed to update LoudDataPath.lua with strategic icon config' end
+    if reps~=1 then feedback:push{{0.7, 0, 0.3}, 'Failed to update LoudDataPath.lua with strategic icon config\n'} end
     return stringData
 end
